f9a2494436b1f4215a03e34ef223e1e9b2c5fd64
Karesz0411 <91952234+Karesz0411@users.noreply.github.com>
2024-05-17 02:19:00 +0200

    xml olvasás és írás kijavítva
    Sikerült kijavítanom az xml olvasást és írást

--- VECB52_hianypotlas/VECB52_0430/DomReadVECB52/src/main/java/hu/the/vecb52/DomReadVECB52.java
+++ VECB52_hianypotlas/VECB52_0430/DomReadVECB52/src/main/java/hu/the/vecb52/DomReadVECB52.java
@@ -1,13 +1,11 @@
-
 package hu.the.vecb52;
 
-/**
- *
- * @author Hallgató
- */
 import java.io.File;
 import java.io.IOException;
 import javax.xml.XMLConstants;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
 import org.xml.sax.SAXException;
@@ -28,53 +26,18 @@ public class DomReadVECB52 {
 
             doc.getDocumentElement().normalize();
 
-            System.out.println("Root Element :" + doc.getDocumentElement().getNodeName());
-            System.out.println("------");
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
 
-            NodeList list = doc.getElementsByTagName("lesson");
+            System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
 
-            for (int temp = 0; temp < list.getLength(); temp++) {
+            DOMSource source = new DOMSource(doc);
+            StreamResult consoleResult = new StreamResult(System.out);
+            transformer.transform(source, consoleResult);
 
-                Node node = list.item(temp);
-
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-
-                    Element element = (Element) node;
-
-                    String id = element.getAttribute("id");
-
-                    String course = element.getElementsByTagName("course").item(0).getTextContent();
-                    String teacher = element.getElementsByTagName("teacher").item(0).getTextContent();
-                    String location = element.getElementsByTagName("location").item(0).getTextContent();
-                    String major = element.getElementsByTagName("major").item(0).getTextContent();
-
-                    NodeList timeNodeList = element.getElementsByTagName("time");
-
-                    if (timeNodeList.getLength() > 0) {
-                        Element timeElement = (Element) timeNodeList.item(0);
-                        String day = timeElement.getElementsByTagName("day").item(0).getTextContent();
-                        String startTime = timeElement.getElementsByTagName("start").item(0).getTextContent();
-                        String endTime = timeElement.getElementsByTagName("end").item(0).getTextContent();
-
-                        System.out.println("Current Element :" + node.getNodeName());
-                        System.out.println("Staff Id : " + id);
-                        System.out.println("Course : " + course);
-                        System.out.println("Teacher : " + teacher);
-                        System.out.println("Location : " + location);
-                        System.out.println("Major : " + major);
-                        System.out.println("Day : " + day);
-                        System.out.println("Start Time : " + startTime);
-                        System.out.println("End Time : " + endTime);
-                        System.out.println();
-                    }
-                    else {
-                        System.out.println("Warning: No time information found for lesson with id: " + id);
-                    }
-                }
-            }
-        } catch (ParserConfigurationException e) {
+        } catch (ParserConfigurationException | TransformerException e) {
             e.printStackTrace();
         }
     }
 }
-
